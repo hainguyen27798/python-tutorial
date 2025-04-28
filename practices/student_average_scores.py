73af696d3dd2eb445d74a4d11738d9ee57f8e3b0
Hai Nguyen <hainguyen27798@gmail.com>
2025-04-28 19:12:11 +0700

    add academic standing into student average scores

--- practices/student_average_scores.py
+++ practices/student_average_scores.py
@@ -11,6 +11,19 @@ class Student:
   def average(self):
     return (self.maths + self.literality + self.english) / 3
 
+  def academic_standing(self):
+    average = self.average()
+    if average >= 85:
+      return "Excellent"
+    elif average >= 70:
+      return "Good"
+    elif average >= 55:
+      return "Average"
+    elif average >= 40:
+      return "Below Average"
+    else:
+      return "Fail"
+
 
 def read_csv(filename):
   student_list = []
@@ -28,7 +41,8 @@ def read_csv(filename):
 
 if __name__ == '__main__':
   students = read_csv('../data/student-subject-scores.csv')
-  print(f"{'Name':<20} {'Average':>8}")
-  print("-" * 30)
+  print(f"{'Name':<20} {'Average':>8} {'Academic Standing':>20}")
+  print("-" * 50)
   for student in students:
-    print("{name:<20} {average:>8.2f}".format(name=student.name, average=student.average()))
+    print("{name:<20} {average:>8.2f} {academic_standing:>20}".format(name=student.name, average=student.average(),
+                                                                      academic_standing=student.academic_standing()))
